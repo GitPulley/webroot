9bd8685975708ce2043c61273ae32b4a783cf91f
Ian Goegebuer <iangoegebuer@gmail.com>
2014-05-25 11:56:05 -0700

    Visiots

---
+++ html/visitors.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pulley</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootystrap.css" rel="stylesheet">
+    <link href="css/site.css" rel="stylesheet">
+    <link rel="icon" type="image/ico" href="assets/favicon.ico">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include 'assets/navbar.html';?>
+    <div id="main" class='container' style="width: 760px; margin: 0 auto">
+      <h1>Visitors</h1>
+<div class="panel panel-default">
+        <div class="panel-body">
+Kevin<br>
+Ryan<br>
+Dennis<br>
+
+    </div>
+    </div>
+    <?php include 'assets/footer.html'?>
+
+    
+    <!-- jQuery (necessary for Bootstrap JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootystrap.min.js"></script>
+  
+    
+
+</body>
+</html>
